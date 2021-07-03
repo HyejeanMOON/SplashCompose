abe8ffaa366c49f892b576e2fe414dc80b0bf39b
MOON <wenhaichen1992@gmail.com>
2021-07-03 19:21:17 +0900

    add no photo logic in favorite screen

--- app/src/main/java/com/hyejeanmoon/splashcompose/screen/favorite/FavoriteScreen.kt
+++ app/src/main/java/com/hyejeanmoon/splashcompose/screen/favorite/FavoriteScreen.kt
@@ -3,24 +3,30 @@ package com.hyejeanmoon.splashcompose.screen.favorite
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.google.accompanist.glide.rememberGlidePainter
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import com.hyejeanmoon.splashcompose.R
 import com.hyejeanmoon.splashcompose.db.FavoritePhoto
 
 @ExperimentalFoundationApi
@@ -39,16 +45,24 @@ fun FavoriteScreen(
         state = rememberSwipeRefreshState(isRefreshing),
         onRefresh = { viewModel.getAllFavoritePhotos() }
     ) {
-        LazyVerticalGrid(
-            modifier = modifier.fillMaxSize().padding(0.dp,0.dp,0.dp,48.dp),
-            cells = GridCells.Adaptive(minSize = 160.dp)
-        ) {
-            favoritePhotoList?.also {
-                items(it) { photo ->
-                    FavoritePhotoItem(
-                        photo = photo,
-                        onFavoritePhotoClick = onFavoritePhotoClick
-                    )
+        favoritePhotoList?.also { photoList ->
+
+            if (photoList.isEmpty()) {
+                NoFavoritePhoto()
+            } else {
+                LazyVerticalGrid(
+                    modifier = modifier
+                        .fillMaxSize()
+                        .padding(0.dp, 0.dp, 0.dp, 48.dp),
+                    cells = GridCells.Adaptive(minSize = 160.dp)
+                ) {
+                    items(photoList) { photo ->
+                        FavoritePhotoItem(
+                            photo = photo,
+                            onFavoritePhotoClick = onFavoritePhotoClick
+                        )
+                    }
+
                 }
             }
         }
@@ -77,4 +91,45 @@ fun FavoritePhotoItem(
         contentDescription = "favorite photo",
         contentScale = ContentScale.Crop
     )
+}
+
+@Composable
+fun NoFavoritePhoto(
+    modifier: Modifier = Modifier
+) {
+
+    ConstraintLayout(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(0.dp, 50.dp, 0.dp, 0.dp)
+    ) {
+
+        val (img, txt) = createRefs()
+
+        Image(
+            modifier = Modifier
+                .size(128.dp)
+                .constrainAs(img) {
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    top.linkTo(parent.top)
+                },
+            painter = painterResource(id = R.mipmap.ic_no_favorite_photos),
+            contentDescription = "no favorite photos"
+        )
+
+        Text(
+            modifier = Modifier
+                .padding(0.dp, 40.dp, 0.dp, 0.dp)
+                .constrainAs(txt) {
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    top.linkTo(img.bottom)
+                },
+            text = "There are not favorite photos!",
+            textAlign = TextAlign.Center,
+            color = Color.Black,
+            fontSize = 16.sp
+        )
+    }
 }
