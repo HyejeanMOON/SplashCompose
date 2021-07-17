bcf282f29414c192908258dfcf97596f55c8cf16
MOON <wenhaichen1992@gmail.com>
2021-07-17 23:56:30 +0900

    fix ui margin

--- app/src/main/java/com/hyejeanmoon/splashcompose/screen/photodetail/PhotoDetailScreen.kt
+++ app/src/main/java/com/hyejeanmoon/splashcompose/screen/photodetail/PhotoDetailScreen.kt
@@ -234,8 +234,8 @@ fun PhotoDetailCreatedTimes(
 ) {
     Row(
         modifier = modifier
-            .requiredHeight(30.dp)
-            .padding(20.dp, 10.dp, 0.dp, 0.dp),
+            .requiredHeight(20.dp)
+            .padding(20.dp, 0.dp, 0.dp, 0.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
@@ -253,7 +253,7 @@ fun PhotoDetailLocation(
 ) {
     Row(
         modifier = modifier
-            .requiredHeight(40.dp)
+            .requiredHeight(30.dp)
             .padding(20.dp, 10.dp, 0.dp, 0.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
