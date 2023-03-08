d9cfbf6fbe6cdd61878ff549b73b4de3729604a3
MOON <wenhaichen1992@gmail.com>
2023-03-09 01:46:04 +0900

    update version code

--- app/src/main/java/com/hyejeanmoon/splashcompose/screen/settings/SettingsScreen.kt
+++ app/src/main/java/com/hyejeanmoon/splashcompose/screen/settings/SettingsScreen.kt
@@ -67,7 +67,7 @@ fun SettingsScreen(
             listOf(
                 SettingItemDetail(
                     title = SettingsItems.VERSION,
-                    content = "1.4.2"
+                    content = "1.4.3"
                 ),
                 SettingItemDetail(
                     title = SettingsItems.LICENSES
